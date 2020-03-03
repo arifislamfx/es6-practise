5186a0e87c4620879311522a1f297ba1026c8f24
arifislamfx <arifislammfx@gmail.com>
2020-03-03 13:35:55 +0800

    added file

---
+++ letConst.js
@@ -0,0 +1,15 @@
+const myName = "Arif";
+console.log(myName);
+
+const num = [12, 15, 18];
+num[2] = 20;
+num.push(16);
+num.pop();
+
+console.log(num);
+
+let name = "sona-mia";
+console.log(name);
+
+let myFriend = "koduAzad";
+console.log(myFriend);
